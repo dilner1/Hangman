d375e6dd864e662b61b6edd242a136d21a4a481d
dilner1 <dan.milner871@gmail.com>
2021-11-30 16:20:49 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,45 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Must include - 
+#   difficulty selector
+#   word for player to player
+#   player turn number
+#   function that draws hangman
+#   function that subtracts player turn on guess
+#   if word is guessed break cycle - start again
+
+word = "guess"
+turns_left = 6
+
+def select_difficulty():
+    """
+    Player chooses difficulty - this will then grab word from one or several databases / text files
+    """
+    pass
+
+def get_word():
+    """
+    Gets word from database
+    """
+    pass
+
+def hide_word():
+    pass
+
+def player_choice():
+    """
+    Player chooses a letter - must be a letter
+    """
+def letter_match():
+    """
+    does player choice match a letter in word
+    also check if letter has already been selected?
+    """
+
+def win():
+    """
+    Have alll letters been selected or goes used?
+    """
+
+def main():
+    """
+    Calls all functions
+    """
