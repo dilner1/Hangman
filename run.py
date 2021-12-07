553806a1c75a0331f9c26642d8dc29bc5ef4bbec
dilner1 <dan.milner871@gmail.com>
2021-12-07 14:22:04 +0000

    Commenting out code, adding convert word to _

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
 import random
 
+word = "words"
 lives = 8
 letters_guessed = []
+done = False
+
 difficulty = ""
 
 logo = ("""
@@ -13,56 +16,89 @@ logo = ("""
                   /____/                        
     """)
 
-def difficulty_select():
-    """
-    Takes input from user to select difficulty
-    """
+while not done:
+    for letter in word:
+        if letter.lower() in guesses:
+            print(letter, end="")
+        else:
+            print("_",end="")
+    print("")
+
+
+
+
+
+
+
+
+# def difficulty_select():
+#     """
+#     Takes input from user to select difficulty
+#     """
     
-    global difficulty
-    difficulty_question = input("Please select difficulty,\n Type easy, medium or hard: ")
-    difficulty += difficulty_question.lower()
-    diff = difficulty.replace(" ", "")
+#     global difficulty
+#     difficulty_question = input("Please select difficulty,\n Type easy, medium or hard: ")
+#     difficulty += difficulty_question.lower()
+#     diff = difficulty.replace(" ", "")
 
-    print(f"You selected {diff}.")
+#     if validate_difficulty(diff):
+#             print(f"You selected {diff}.")
+            
+#     # if diff != 'easy':
+#     #     print('You need to type easy, medium or hard.')
 
 
-def generate_the_word():
-    """
-    Opens easyWords.txt file and selects a word at random
-    """
-    print(difficulty)
-    word_file = open("easyWords.txt", "r")
-    random_word = random.choice(word_file.read().split('\n'))
-    word_file.close()
-    print(random_word.lower()) 
-
-def player_turn():
-    """
-    Takes player guess
-    """
-    letters_guessed 
-    player_guess = input("Please choose a letter:\n").lower()
-    if not player_guess.isalpha():
-        return "Only letters are allowed, try again!"
-    else:
-        print(f'You selected the letter {player_guess}')
-        letters_guessed.append(player_guess)
-        print(letters_guessed)
+#     # else:
+#     #     print(f"You selected {diff}.")
         
-def main():
-    """
-    Calls all functions
-    """
-    print("Welcome to Hangman, let's begin!")
-    difficulty_select()
-    generate_the_word()
-    print("A word has been selected.")
-    player_turn()
+# def validate_difficulty(value):
+#     try:
+#         value == 'easy'
+#     except ValueError as e:
+#         print(f"You typed the wrong thing, {e}")
+#     except Exeption:
+#         print('THIS IS WRONG')
 
-print(logo)
-main()
+
+# def generate_the_word():
+#     """
+#     Opens easyWords.txt file and selects a word at random
+#     """
+#     # print(difficulty)
+#     word_file = open("easyWords.txt", "r")
+#     random_word = random.choice(word_file.read().split('\n'))
+#     word_file.close()
+#     print(random_word.lower()) 
+
+# def player_turn():
+#     """
+#     Takes player guess
+#     """
+#     letters_guessed 
+#     player_guess = input("Please choose a letter:\n").lower()
+#     if not player_guess.isalpha():
+#         return "Only letters are allowed, try again!"
+#     else:
+#         print(f'You selected the letter {player_guess}')
+#         letters_guessed.append(player_guess)
+#         print(letters_guessed)
+        
+# def main():
+#     """
+#     Calls all functions
+#     """
+#     print("Welcome to Hangman, let's begin!")
+#     difficulty_select()
+#     generate_the_word()
+#     print("A word has been selected.")
+#     player_turn()
+
+# print(logo)
+# main()
 
     
+
+
 # def hang_man_drawing(image):
 #     """"
 #     Stores the drawing states of the hangman
