85b2977ebb3be1e075bd8f452259e77612ed413c
dilner1 <dan.milner871@gmail.com>
2021-12-10 15:15:59 +0000

    Adding to readme

--- run.py
+++ run.py
@@ -6,21 +6,24 @@ all_easy_words = easyWords.easy_word_list
 word = random.choice(all_easy_words)
 hangman = drawing.drawing_dictionary
 
+
+done = False
+
+# difficulty = ""
+
 logo = ("""
     __  __                                      
    / / / /___ _____  ____ _____ ___  ____ _____ 
   / /_/ / __ `/ __ \/ __ `/ __ `__ \/ __ `/ __ \\
  / __  / /_/ / / / / /_/ / / / / / / /_/ / / / /
 /_/ /_/\__,_/_/ /_/\__, /_/ /_/ /_/\__,_/_/ /_/ 
-                  /____/                          
+                  /____/                        
     """)
-
-
 
 class game():
     
 
-    
+
 def main():
     """
     Prints all functions
